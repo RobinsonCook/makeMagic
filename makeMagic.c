f822b13fbb124cf11a64094930b7c85ef1ba4fe0
Robinson Cook <48704539+RobinsonCook@users.noreply.github.com>
2020-02-16 21:54:17 -0600

    formatting

--- makeMagic.c
+++ makeMagic.c
@@ -46,7 +46,7 @@ int getSize()
     printf("Enter magic square's size (odd integer >=3)");
     char buffer[5];
     fgets(buffer, 5, stdin);
-    printf("input was: %n\n", buffer);
+    printf("input was: %c\n", buffer);
 
     return 0;
 }
