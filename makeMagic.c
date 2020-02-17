38888c8f6c5ce8622b09c777aa5fdf8dc16a6f20
Robinson Cook <48704539+RobinsonCook@users.noreply.github.com>
2020-02-16 21:34:34 -0600

    Create makeMagic.c

---
+++ makeMagic.c
@@ -0,0 +1,84 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2020 Jim Skrentny
+// Posting or sharing this file is prohibited, including any changes/additions.
+//
+////////////////////////////////////////////////////////////////////////////////
+// Main File:        (name of file with main function)
+// This File:        (name of this file)
+// Other Files:      (name of all other files if any)
+// Semester:         CS 354 Spring 2020
+//
+// Author:           (your name)
+// Email:            (your wisc email address)
+// CS Login:         (your CS login name)
+//
+/////////////////////////// OTHER SOURCES OF HELP //////////////////////////////
+//                   Fully acknowledge and credit all sources of help,
+//                   other than Instructors and TAs.
+//
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   Avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of
+//                   of any information you find.
+////////////////////////////////////////////////////////////////////////////////
+   
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Structure that represents a magic square
+typedef struct {
+    int size;     // dimension of the square
+    int **msquare; // pointer to heap allocated magic square
+} MSquare;
+
+/* TODO:
+ * Prompts the user for the magic square's size, reads it,
+ * checks if it's an odd number >= 3 (if not display the required
+ * error message and exit), and returns the valid number.
+ */
+int getSize() {
+    return 0;   
+} 
+   
+/* TODO:
+ * Makes a magic square of size n using the Siamese algorithm
+ * and returns a pointer to the completed MSquare struct.
+ *
+ * n the number of rosw and columns
+ */
+MSquare *generateMSquare(int n) {
+    return NULL;    
+} 
+
+/* TODO:  
+ * Opens a new file (or overwrites the existing file)
+ * and writes the square in the specified format.
+ *
+ * msquare the magic square to output
+ * filename the name of the output file
+ */
+void outputMSquare(MSquare *msquare, char *filename) {
+}
+
+/* TODO:
+ * Generates a magic square of the user specified size and
+ * output the quare to the output filename
+ */
+int main(int argc, char *argv[]) {
+    // TODO: Check input arguments to get output filename
+
+    // TODO: Get magin square's size from user
+
+    // TODO: Generate the magic square
+
+    // TODO: Output the magic square
+
+    return 0;
+} 
+
+
+   
