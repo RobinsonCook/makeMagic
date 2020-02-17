d70db21e6c3c77ef711a316ef9fa72773d5db949
Robinson Cook <48704539+RobinsonCook@users.noreply.github.com>
2020-02-17 01:41:47 -0600

    f

--- makeMagic.c
+++ makeMagic.c
@@ -43,7 +43,7 @@ void fill(int **board, int size)
     int i = 0;
     //get the middle of the board
     int k = size / 2;
-    printf("%d:%d:%d", k, i, n)
+    printf("%d:%d:%d", k, i, n);
 }
 /* TODO:
  * Prompts the user for the magic square's size, reads it,
